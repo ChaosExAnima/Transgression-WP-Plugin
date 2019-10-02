40c63f4f1557dc8362dddf02a21fb56196522b92
Echo G <ChaosExAnima@users.noreply.github.com>
2019-10-02 14:38:57 -0400

    Additional WC logic
    Removes category breadcrumb, adds login prompt for new users.

--- transgression.php
+++ transgression.php
@@ -30,10 +30,15 @@ class Core {
 		add_filter( 'wpa_change_form_label', [ $this, 'filter_wpa_login_label' ] );
 
 		// WooCommerce plugin.
-		add_filter( 'wc_get_template', [ $this, 'filter_woo_template' ], 10, 2 );
-		add_filter( 'woocommerce_account_menu_items', [ $this, 'filter_woo_account_menu' ] );
-		add_filter( 'woocommerce_save_account_details_required_fields', [ $this, 'filter_woo_account_required' ] );
-		add_filter( 'woocommerce_add_to_cart_validation', [ $this, 'filter_woo_cart_validate' ], 10, 2 );
+		add_filter( 'wc_get_template', [ $this, 'filter_wc_template' ], 10, 2 );
+		add_filter( 'woocommerce_account_menu_items', [ $this, 'filter_wc_account_menu' ] );
+		add_filter( 'woocommerce_save_account_details_required_fields', [ $this, 'filter_wc_account_required' ] );
+		add_filter( 'woocommerce_add_to_cart_validation', [ $this, 'filter_wc_cart_validate' ], 10, 2 );
+		add_filter( 'woocommerce_breadcrumb_product_terms_args', [ $this, 'filter_wc_remove_category_breadcrumb' ] );
+		add_action( 'woocommerce_before_single_product_summary', [ $this, 'action_wc_show_login' ] );
+
+		remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
+		add_action( 'woocommerce_single_variation', [ $this, 'action_wc_login_add_cart' ], 20 );
 
 		// Core WP.
 		add_filter( 'insert_user_meta', [ $this, 'filter_wp_save_pronouns' ] );
@@ -78,7 +83,7 @@ class Core {
 	 * @param string $template_name The template name.
 	 * @return string
 	 */
-	public function filter_woo_template( string $template, string $template_name ) : string {
+	public function filter_wc_template( string $template, string $template_name ) : string {
 		if ( 'myaccount/form-login.php' === $template_name ) {
 			$template = '';
 		} elseif ( 'myaccount/dashboard.php' === $template_name ) {
@@ -98,7 +103,7 @@ class Core {
 	 * @param array $menu Array of menu items.
 	 * @return array
 	 */
-	public function filter_woo_account_menu( array $menu ) : array {
+	public function filter_wc_account_menu( array $menu ) : array {
 		$menu['edit-account'] = __( 'Edit Info', 'transgression' );
 		return $menu;
 	}
@@ -109,7 +114,7 @@ class Core {
 	 * @param array $fields Array of user account fields.
 	 * @return array
 	 */
-	public function filter_woo_account_required( array $fields ) : array {
+	public function filter_wc_account_required( array $fields ) : array {
 		unset( $fields['account_first_name'] );
 		unset( $fields['account_last_name'] );
 		return $fields;
@@ -122,9 +127,15 @@ class Core {
 	 * @param integer $product_id The product ID being added.
 	 * @return boolean
 	 */
-	public function filter_woo_cart_validate( bool $valid, int $product_id ) : bool {
+	public function filter_wc_cart_validate( bool $valid, int $product_id ) : bool {
 		if ( ! $valid ) {
 			return $valid;
+		}
+
+		// Ensure that the user is logged in.
+		if ( ! is_user_logged_in() ) {
+			wc_add_notice( 'You must be logged in first.', 'error' );
+			return false;
 		}
 
 		// Ensure the product is not already in the cart.
@@ -137,14 +148,46 @@ class Core {
 		}
 
 		// If the user is logged in, verify whether they've previously bought a ticket.
-		if ( is_user_logged_in() ) {
-			if ( wc_customer_bought_product( '', get_current_user_id(), $product_id ) ) {
-				wc_add_notice( 'You have already purchased a ticket to this event.', 'error' );
-				return false;
-			}
+		if ( wc_customer_bought_product( '', get_current_user_id(), $product_id ) ) {
+			wc_add_notice( 'You have already purchased a ticket to this event.', 'error' );
+			return false;
 		}
 
 		return $valid;
+	}
+
+	/**
+	 * Removes category breadcrumb from single product page.
+	 *
+	 * @param array $query_args Term query args.
+	 * @return array
+	 */
+	public function filter_wc_remove_category_breadcrumb( array $query_args ) : array {
+		$query_args['include'] = [ -1 ];
+		return $query_args;
+	}
+
+	/**
+	 * Shows passwordless login on single product page.
+	 *
+	 * @return void
+	 */
+	public function action_wc_show_login() {
+		if ( ! is_user_logged_in() ) {
+			// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo wpa_front_end_login();
+		}
+	}
+
+	/**
+	 * Shows add to cart button only if user is logged in.
+	 *
+	 * @return void
+	 */
+	public function action_wc_login_add_cart() {
+		if ( is_user_logged_in() ) {
+			woocommerce_single_variation_add_to_cart_button();
+		}
 	}
 
 	/**
